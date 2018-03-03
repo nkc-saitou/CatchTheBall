3265bbb791cffe035f1aade64c0e25e72a940d26
shigeyama <nkc.wataru.shigeyama@gmail.com>
2018-03-03 20:25:11 +0900

    あたり判定

--- CatchTheBall/Classes/CollisionManager.h
+++ CatchTheBall/Classes/CollisionManager.h
@@ -1,1 +1,21 @@
 #pragma once
+#include <vector>
+#include "Collision.h"
+#include "Singleton.h"
+#include "Object.h"
+
+class Collision;
+class CollisionManager : public Singleton<CollisionManager>
+{
+
+public:
+	std::vector<Collision*> collision;
+	void HitCollision();
+	void hit(Collision* a, Collision* b);
+
+private:
+
+	bool HitBoxToBox(Collision firstCollision, Collision secondCollision);
+	bool HitCircleToCircle(Collision firstCollision, Collision secondCollision);
+	bool HitCircleToBox(Collision firstCollision, Collision secondCollision);
+};
